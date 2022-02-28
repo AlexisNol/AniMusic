a3804d77dc468df658d37d8388b07bcf3d219dbc
AlexisNol <78601828+AlexisNol@users.noreply.github.com>
2022-02-28 10:11:17 +0100

    Add files via upload

--- components/liste-anime/liste-anime.component.ts
+++ components/liste-anime/liste-anime.component.ts
@@ -18,7 +18,7 @@ export class ListeAnimeComponent implements OnInit {
 //tabAnime animes[];
 
 function cleanString(uneChaine: string){
-	chaine = uneChaine.toLowerCase();
+	let chaine = uneChaine.toLowerCase();
 	chaine.replace("/[^a-z0-9_'\s-]/", "");
 	chaine.replace("/[\s-]+/", " ");
 	chaine.replace("/[\s_]/", " ");
@@ -31,36 +31,34 @@ function topAnime(animes: tabAnime[]){
   console.log(animes);
 }
 
-function animeByDate(animes: tabAnime[]){
+function animeByDate(animes: tabAnime[], annee: number, saison: string){
   /* Requête à faire pour afficher les animes sorties lors d'une saison et année précise */
-	if(isset($_POST['annee']) && !empty($_POST['annee']) && (isset($_POST['saison']) && !empty($_POST['saison'])) {
-		let anneeRecherche = $_POST['annee'];
-		let saisonRecherche = this.cleanString($_POST['saison']);
+	if(annee !== null && saison !== null) {
+		let saisonRecherche = this.cleanString(saison);
     switch (saisonRecherche){
       case "hiver":saisonRecherche = "winter";break;
       case "printemps":saisonRecherche = "spring";break;
       case "ete":saisonRecherche = "summer";break;
       case "automne":saisonRecherche = "fall";break;
     }
-		var urlSortieSaison = 'https://api.jikan.moe/v3/season/'+ anneeRecherche +'/'+ saisonRecherche;
+		var urlSortieSaison = 'https://api.jikan.moe/v3/season/'+ annee +'/'+ saisonRecherche;
 		animes = request.open('GET', urlSortieSaison);
     console.log(animes);
 	}else{
-		echo "Impossible d'effectuer cette recherche, les paramètres sont incorrectes...";
+		console.log("Impossible d'effectuer cette recherche, les paramètres sont incorrectes...");
 	}
 
 }
 
-function searchAnime(animes: tabAnime[]){
-  if(isset($_POST['mot']) && !empty($_POST['mot'])) {
-	  let motRecherche = this.cleanString($_POST['mot']);
-
+function searchAnime(animes: tabAnime[], mot: string){
+  if(mot !== null) {
+	  let motRecherche = this.cleanString(mot);
 	  /* Requête à faire pour rechercher un anime précis */
 	  var txtSearch = motRecherche.replace(" ", "%20");
     var urlS = "https://api.jikan.moe/v3/search/anime?q=" + txtSearch + "&order_by=title&sort=asc&limit=10";
 	  animes = request.open('GET', urlS);
     console.log(animes);
-  }else {
-    echo "Aucune recherche effectuée...";
+  }else{
+    console.log("Aucune recherche effectuée...");
   }
 }
