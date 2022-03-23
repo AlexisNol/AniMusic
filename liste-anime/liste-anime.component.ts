8e9afae15d9d7e4283dda8aeccfd88cd53606dcf
AlexisNol <78601828+AlexisNol@users.noreply.github.com>
2022-03-23 23:38:35 +0100

    Add files via upload

--- liste-anime/liste-anime.component.ts
+++ liste-anime/liste-anime.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 import { StructAnimeComponent } from './struct-anime';
+import { NONE_TYPE } from '@angular/compiler';
+import { Component, HostListener, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-liste-anime',
   templateUrl: './liste-anime.component.html',
@@ -8,15 +12,17 @@ import { StructAnimeComponent } from './struct-anime';
 
 export class ListeAnimeComponent implements OnInit {
 
-  annee: number;
-  saison: string;
-  mot: string;
-  animes: StructAnimeComponent[];
+  @Input() annee: number;
+  @Input() saison: string;
+  @Input() mot: string;
+  @Input() responseText: string;
+  //@Input() animes: StructAnimeComponent[];
 
-  constructor(uneAnnee: number, uneSaison: string, unMot: string) {
-    this.annee = uneAnnee;
-    this.saison = uneSaison;
-    this.mot = unMot;
+  constructor() {
+    this.annee = 0;
+    this.saison = "";
+    this.mot = "";
+    this.responseText = "api_jikan.json";
   }
 
   ngOnInit(): void {
@@ -24,7 +30,7 @@ export class ListeAnimeComponent implements OnInit {
 
   //tabAnime animes[];
 
-  public cleanString(uneChaine: string){
+  cleanString(uneChaine: string){
   	let chaine = uneChaine.toLowerCase();
   	chaine.replace("/[^a-z0-9_'\s-]/", "");
   	chaine.replace("/[\s-]+/", " ");
@@ -32,16 +38,31 @@ export class ListeAnimeComponent implements OnInit {
   	return chaine;
   }
 
-  public topAnime(){
+  topAnime(){
+
+    console.log("Recherche d'anime en cours");
+
     /* Requête à faire pour afficher les animes en vedette en ce moment */
-    this.animes = [];
-  	this.animes.push(fetch("https://api.jikan.moe/v3/top/anime/1/bypopularity"));
-    console.log(this.animes);
+    request.open('GET','https://api.jikan.moe/v3/top/anime/1/bypopularity');
+    var fichJSON = JSON.parse(this.responseText);
+    //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+    //let parent_div = $('#anim_2');
+    //parent_div.html(iframe);
+    console.log(fichJSON);
+
   }
 
-  public animeByDate(){
+  animeByDate(){
+
+    console.log("Recherche d'anime en cours");
+
+    this.annee = $('#annee').val();
+    this.saison = $('#saison').val();
+    this.saison = this.saison.toString();
+
     /* Requête à faire pour afficher les animes sorties lors d'une saison et année précise */
   	if(this.annee !== null && this.saison !== null) {
+      
   		let saisonRecherche = this.cleanString(this.saison);
       switch (saisonRecherche){
         case "hiver":saisonRecherche = "winter";break;
@@ -54,33 +75,53 @@ export class ListeAnimeComponent implements OnInit {
           }
       }
   		var urlSortieSaison = "https://api.jikan.moe/v3/season/"+ this.annee +"/"+ saisonRecherche;
-      this.animes = [];
-  		this.animes.push(fetch(urlSortieSaison));
-      console.log(this.animes);
+      //this.animes = [];
+
+  		request.open('GET',urlSortieSaison);
+      var fichJSON = JSON.parse(this.responseText);
+      //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+      //let parent_div = $('#anim_4');
+      //parent_div.html(iframe);
+      console.log(fichJSON);
   	}else{
   		console.log("Impossible d'effectuer cette recherche, les paramètres sont incorrectes...");
   	}
 
   }
 
-  public searchAnime(){
+  searchAnime(){
+
+    console.log("Recherche d'anime en cours");
+
+    this.mot = $('#nom').val();
+    this.mot = this.mot.toString();
+
     if(this.mot !== null) {
   	  let motRecherche = this.cleanString(this.mot);
   	  /* Requête à faire pour rechercher un anime précis */
   	  var txtSearch = motRecherche.replace(" ", "%20");
       var urlS = "https://api.jikan.moe/v3/search/anime?q=" + txtSearch + "&order_by=title&sort=asc&limit=10";
-      this.animes = [];
-      this.animes.push(fetch(urlS));
-      console.log(this.animes);
-    }else{
-      console.log("Aucune recherche effectuée...");
+      
+      request.open('GET',urlS);
+      var fichJSON = JSON.parse(this.responseText);
+      //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+      //let parent_div = $('#anim_1');
+      //parent_div.html(iframe);
+      console.log(fichJSON);
     }
   }
 
-  public newAnimes(){
+  newAnimes(){
+
+    console.log("Recherche d'anime en cours");
+
     /* Requête qui affiche en recommandation les animes récents */
-    this.animes = [];
-    this.animes.push(fetch("https://api.jikan.moe/v4/recommendations/anime"));
-    console.log(this.animes);
+    //this.animes = [];
+    request.open('GET','https://api.jikan.moe/v4/recommendations/anime');
+    var fichJSON = JSON.parse(this.responseText);
+    //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+    //let parent_div = $('#anim_3');
+    //parent_div.html(iframe);
+    console.log(fichJSON);
   }
 }
