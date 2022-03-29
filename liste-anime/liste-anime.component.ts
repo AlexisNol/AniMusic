9189657c9ef69aa56b8e4556543091abef2dd8db
AlexisNol <78601828+AlexisNol@users.noreply.github.com>
2022-03-29 16:15:16 +0200

    Add files via upload

--- liste-anime/liste-anime.component.ts
+++ liste-anime/liste-anime.component.ts
@@ -58,11 +58,11 @@ export class ListeAnimeComponent implements OnInit {
 
     this.annee = $('#annee').val();
     this.saison = $('#saison').val();
-    this.saison = this.saison.toString();
+    this.saison.toString();
 
     /* Requête à faire pour afficher les animes sorties lors d'une saison et année précise */
   	if(this.annee !== null && this.saison !== null) {
-      
+
   		let saisonRecherche = this.cleanString(this.saison);
       switch (saisonRecherche){
         case "hiver":saisonRecherche = "winter";break;
@@ -75,7 +75,6 @@ export class ListeAnimeComponent implements OnInit {
           }
       }
   		var urlSortieSaison = "https://api.jikan.moe/v3/season/"+ this.annee +"/"+ saisonRecherche;
-      //this.animes = [];
 
   		request.open('GET',urlSortieSaison);
       var fichJSON = JSON.parse(this.responseText);
@@ -94,20 +93,23 @@ export class ListeAnimeComponent implements OnInit {
     console.log("Recherche d'anime en cours");
 
     this.mot = $('#nom').val();
-    this.mot = this.mot.toString();
+    this.mot.toString();
 
     if(this.mot !== null) {
   	  let motRecherche = this.cleanString(this.mot);
   	  /* Requête à faire pour rechercher un anime précis */
   	  var txtSearch = motRecherche.replace(" ", "%20");
       var urlS = "https://api.jikan.moe/v3/search/anime?q=" + txtSearch + "&order_by=title&sort=asc&limit=10";
-      
+
       request.open('GET',urlS);
       var fichJSON = JSON.parse(this.responseText);
+      var titre = fichJSON.results[0].title;
+
       //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
       //let parent_div = $('#anim_1');
       //parent_div.html(iframe);
-      console.log(fichJSON);
+
+      console.log(titre);
     }
   }
 
@@ -116,7 +118,6 @@ export class ListeAnimeComponent implements OnInit {
     console.log("Recherche d'anime en cours");
 
     /* Requête qui affiche en recommandation les animes récents */
-    //this.animes = [];
     request.open('GET','https://api.jikan.moe/v4/recommendations/anime');
     var fichJSON = JSON.parse(this.responseText);
     //let iframe = `<div class='song'><iframe src=${request} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
@@ -124,4 +125,5 @@ export class ListeAnimeComponent implements OnInit {
     //parent_div.html(iframe);
     console.log(fichJSON);
   }
+
 }
